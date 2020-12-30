d6d2298ef10501ab9b9a0b2f40de095ac4e5d821
Carl Schaffer <cfmschaffer@hotmail.de>
2020-12-31 00:35:22 +0100

    updating data

--- guitar_scraping/scrape.sh
+++ guitar_scraping/scrape.sh
@@ -3,4 +3,4 @@ source ~/anaconda3/bin/activate
 conda activate sdc
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 python $DIR/scrape.py
-python $DIR/fill_db.py
+python $DIR/fill_db.py $(date +%Y-%m-%d)
