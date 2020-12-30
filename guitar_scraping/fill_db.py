8041819883e87125b1b1ff58e20137dc6a52e97e
Carl Schaffer <cfmschaffer@hotmail.de>
2020-12-31 00:33:50 +0100

    minor changes to scrape script

--- guitar_scraping/fill_db.py
+++ guitar_scraping/fill_db.py
@@ -6,14 +6,16 @@ Created by schaffer at 11/22/19
 
 """
 from pathlib import Path
+import sys
 
 from guitar_scraping.clean_data import get_guitars, get_sales
 from guitar_scraping.db_interface import add_df_to_db
 from guitar_scraping.db_interface.data_models import GuitarInfo, SalesInfo
 from tqdm import tqdm
 
-directory = Path(__file__).parents[1] / "data"
-data_dirs = [*directory.glob("????-??-??")]
+directory = Path(__file__).absolute().parents[1] / "data"
+pattern = sys.argv[1] if len(sys.argv) > 1 else "????-??-??"
+data_dirs = [*directory.glob(pattern)]
 
 progress_bar = tqdm(data_dirs)
 for day in progress_bar:
